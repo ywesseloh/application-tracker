7e1eab57fd3fa77bedba119c7aef2e278ea1e84e
Yannick Wesseloh <y.wesseloh@gmail.com>
2026-07-23 13:02:34 +0200

    Init project

---
+++ backend/src/main/java/com/example/application_tracker/ApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.application_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/application_tracker/ApplicationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.application_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
