4f46dbce8228c05985e0e2c1afc68d2b400e3318
Yannick Wesseloh <y.wesseloh@gmail.com>
2026-07-24 11:38:12 +0200

    Create initial application board

---
+++ frontend/src/ApplicationBoard.tsx
@@ -0,0 +1,8 @@
+export default function ApplicationBoard() {
+  return (
+    <div className="application-board">
+      <h1>Application Board</h1>
+      <p>This is the application board component.</p>
+    </div>
+  )
+}
