0630f4f7693e3437dc915fdc0dc08bbffa074e00
Yannick Wesseloh <y.wesseloh@gmail.com>
2026-09-06 15:05:26 +0200

    Remove unused dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { fileURLToPath, URL } from 'node:url'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    react(), 
-    svgr()
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
